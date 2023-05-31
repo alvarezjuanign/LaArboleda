f90f490039b30b2e31562e6df98286276697fd9c
Juan Alvarez <alvarez.juanign@gmail.com>
2023-05-31 00:11:13 -0300

    Dark mode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Proyecto-UTN
 Proyecto utn, tp final
+Landing page hecha en HTML, CSS y JS de un complejo de cabañas que se encuentran en San Luis, Argentina en la localidad de Carpinteria.
--- main.js
+++ main.js
@@ -6,14 +6,22 @@ const modal = document.getElementById('modal')
 const modalImg = document.getElementById('modal-img')
 const overlay = document.getElementById('overlay')
 
+const darkModeButton = document.getElementById('darkModeButton')
+const body = document.body
+
 const yearCopy = document.getElementById('year')
 yearCopy.textContent = new Date().getFullYear()
+
+
+darkModeButton.addEventListener('click', function () {
+    body.classList.toggle('dark-mode')
+});
 
 Notification.requestPermission()
 if (Notification.permission === 'granted') {
     const options = {
         body: 'Bienvenido a La Arboleda',
-        icon: './images/house-solid.avif'
+        icon: ''
     }
         new Notification('¡Hola!', options);
 }
